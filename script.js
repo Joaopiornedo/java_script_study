163209f2dcac4344678257cf27a69d3634643214
Hello World <joaop.codes@gmail.com>
2024-07-18 00:25:04 -0300

    atualização
    aplicando conceitos basicos de js como variaveis , manipulação de number e strings  e como armazenar uma informação de um prompt em uma variavel.

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
+// Criando Variaveis e convertendo elas para Number :
+// Number.parseInt - Number.parseFloat -- conversão numeros
+//É possivel utilizar apenas o Number inves de .parsetInt/Float
+// Para converter para String : String 
+// prompt == input 
+// var / let == variaveis
+var n1 = Number.parseInt(prompt('Digite um número: '))
+var n2 = Number.parseInt(prompt('Digite outro número: '))
+var soma = n1 + n2 
+//alert == um alerta que aparece na tela do usuario
+alert(`A soma de ${n1} + ${n2} de : ${soma}` )
+// TEM QUE SE UTILIZAR : `   ` ESSA ACENTUAÇÃO
 
-const divs = document.querySelectorAll('.bloco div');
+/* Uma variavel formatada que no python se usa o f aqui se monta dessa forma, Exemplo :
 
-divs.forEach(div => {
-    div.addEventListener('mousemove', () => {
-        div.style.backgroundColor = 'blue'; // Nova cor
-        div.style.width = '250px'; // Novo tamanho (largura)
-        div.style.height = '250px'; // Novo tamanho (altura)
-    });
+var nome = "João"
+var idade = 26
+var altura = 1.88
+imaginamos estar monatndo um print :
+'O aluno ${nome} tem ${idade}anos, e ${altura} de altura'
 
-    div.addEventListener('mouseout', () => {
-        div.style.backgroundColor = 'black'; // Cor original
-        div.style.width = '250px'; // Tamanho original (largura)
-        div.style.height = '250px'; // Tamanho original (altura)
-    });
-});
+o resultado seria :
+' O aluno João tem 26 anos, e 1.88 de altura '
+*/
